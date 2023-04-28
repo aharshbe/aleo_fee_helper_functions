be58201bc9b3ce8f27d1a5b05a66a43d9e10ae6f
a h <austin@aleo.org>
2023-04-28 14:38:44 -0500

    Create bash_helper_functions.sh
    Use these functions in conjunction with the fee model outlined in the [Aleo docs, for the tx fee aleo concept](https://developer.aleo.org/concepts/transaction_fees).

---
+++ bash_helper_functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# determine size of file in kilobytes
+sizeinkb(){
+	size=$(find . -type f -exec ls -l {} \; | awk '{sum += $5} END {print sum}') 
+	echo "$((${size} * .0000001)) KB"
+}
+
+# determine number of aleo credits
+aleocredits(){
+	sizeinkb | awk '{$1/=1024;printf "you should pay %.2f Aleo credits\n",$1}'
+}
+
+# convert credits to gates
+convertcredits(){
+	sizeinkb | awk '{$1/=1024;printf "%.2f\n",$1}' | awk '{$1*=10000000;printf "your fee in gates is %.0f\n",$1}'
+}
+
+# list all fees
+lsaleofees(){
+	sizeinkb
+	aleocredits
+	convertcredits
+}
+
+# EOF
